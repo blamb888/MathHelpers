c021da9db5c0da611599a8c2c009579d574737ed
Brandon Lamb <blamb888@gmail.com>
2023-03-10 21:25:34 +0900

    add line char functionality

--- mean_variance_SD.py
+++ mean_variance_SD.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def estimate_statistics(nums):
     n = len(nums)
     mean = sum(nums) / n
@@ -5,9 +7,17 @@ def estimate_statistics(nums):
     std_dev = variance ** 0.5
     return mean, variance, std_dev
 
+def draw_line_graph(x, y, title, xlabel, ylabel):
+    plt.plot(x, y)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
 # Example usage
 numbers = [1, 2, 3, 4, 5]
 mean, variance, std_dev = estimate_statistics(numbers)
 print(f"Mean: {mean:.2f}")
 print(f"Variance: {variance:.2f}")
 print(f"Standard deviation: {std_dev:.2f}")
+draw_line_graph(['Mean', 'Variance', 'Standard deviation'], [mean, variance, std_dev], 'Statistics', 'Type', 'Value')
